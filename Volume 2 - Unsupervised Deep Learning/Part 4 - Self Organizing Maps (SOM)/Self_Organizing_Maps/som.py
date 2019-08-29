2a89226b309ba2484e21684622440777e8e4cec0
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-08-29 13:13:37 +0200

    starting the SOM

--- Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Self_Organizing_Maps/som.py
+++ Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Self_Organizing_Maps/som.py
@@ -5,10 +5,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+'''In the dataset, all the customers are the input of the NN, they are going to be mapped
+   to a new output space. Between the input/output space we have this NN composed of neurons, 
+   each neuron being initialised as a vector of weights that is the same size as the vector of 
+   customer, a vector of 15 elements (customer ID + 14 attributes) and for each observation 
+   point/customer, the output of the customer will be the neuron that is the closest to the customer.
+   Bascially in the neuron we pick the cluster that is closest to the customer. This neuron is called
+   the winning node. For each customer, the winning node is the most similar neuron to the customer.
+   Then we use a neighbour function to update the weights of the neighbours to move them closer
+   to the point .
+   This is done for all customers in the input space, and everytime it is done the output space
+   decreases and loses dimensions until it reaches a point where the output space stops decreasing.
+   That's the moment where the 2D map is obtained with all the winning nodes that were eventually
+   identified.
+   
+   MID - Mean Interneuron Distance, for each neuron, the mean of the Euclidean distance will be 
+   computed between this neuron and the neurons in its neighbourhood. We define the neighbourhood
+   manually for each neuron. By doing this, we detect outliers because they will be far from all
+   the neurons in its neighbourhood.
+   Then, an inverse mapping function will identify which customers originally in the input space are associated
+   to this winning node, the outlier.
+'''
+
 # Importing the dataset
 dataset = pd.read_csv('Credit_Card_Applications.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, :-1].values #all rows, all columns except the last one - the class
+y = dataset.iloc[:, -1].values #all rows, only the last column
+'''Since the last attribute of the dataset is the class (if the customer's application was succesful-1 or
+   not-0), we separate that column from the dataset
+'''
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
@@ -37,6 +62,9 @@ for i, x in enumerate(X):
          markerfacecolor = 'None',
          markersize = 10,
          markeredgewidth = 2)
+plt.title('Red circles -ve class, Green squares +ve class')
+#plt.xlabel('Time')
+#plt.ylabel('Google Stock Price')
 show()
 
 # Finding the frauds
