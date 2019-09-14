9487d810d26941da7ea0ae27ba71b2017678a742
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-09-14 13:29:45 +0100

    Update keras_first_network.py

--- Practice examples/keras_first_network.py
+++ Practice examples/keras_first_network.py
@@ -15,3 +15,15 @@ dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
 # split into input (X) and output (y) variables
 X = dataset[:,0:8]
 y = dataset[:,8]
+
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
